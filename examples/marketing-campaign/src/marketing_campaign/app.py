a7aa45ae229dac8d37edb989052f18cb1afe6955
Giovanni Conte <giconte@cisco.com>
2025-03-25 18:38:31 +0100

    fix mas app input

--- examples/marketing-campaign/src/marketing_campaign/app.py
+++ examples/marketing-campaign/src/marketing_campaign/app.py
@@ -3,14 +3,14 @@
 import copy
 import os
 
-from agntcy_acp.langgraph.api_bridge import APIBridgeAgentNode, APIBridgeInput, APIBridgeOutput
-from agntcy_acp.langgraph.io_mapper import add_io_mapped_edge, add_io_mapped_conditional_edge
+from agntcy_acp.langgraph.api_bridge import APIBridgeAgentNode, APIBridgeInput
+from agntcy_acp.langgraph.io_mapper import add_io_mapped_conditional_edge
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.state import CompiledStateGraph
 from marketing_campaign import mailcomposer
 from  marketing_campaign import state
 from agntcy_acp.langgraph.acp_node import ACPNode
-from agntcy_acp import ApiClientConfiguration
+from agntcy_acp.acp_v0.configuration import Configuration
 from langchain_core.runnables.graph import MermaidDrawMethod
 from langchain_core.runnables import RunnableConfig
 from langchain_openai.chat_models.azure import AzureChatOpenAI
