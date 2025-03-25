de3f9aca57542a469a9ed65d164387c9dc311973
Jeff Napper <103025963+jnapper7@users.noreply.github.com>
2025-03-25 11:26:31 +0100

    feat: Add model docs script (#43)
    * feat: add script to generate model sphinx docs.
    
    * fix: changed models to sections.

---
+++ scripts/generate_model_autodoc.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Copyright AGNTCY Contributors (https://github.com/agntcy)
+# SPDX-License-Identifier: Apache-2.0
+#
+# Note: This script generates the sphinx rst docs for the 
+# package models.
+
+echo "API Models
+===============
+"
+
+for model in agntcy_acp/acp_v0/models/[a-z]*.py ; do
+    awk -f - "${model}" <<'EOF'
+/^class/ && /BaseModel/ { 
+    match($2, "[^(]+");
+    MODEL = substr($2,RSTART,RLENGTH-RSTART+1);
+    printf("\`\`%s\`\`\n", MODEL);
+    print("***************************************");
+    printf(".. autopydantic_model:: agntcy_acp.models.%s\n", MODEL);
+    print("   :members:");
+    print("   :undoc-members:");
+    print("   :show-inheritance:\n");
+}
+/^class/ && /Enum/ { 
+    match($2, "[^(]+");
+    MODEL = substr($2,RSTART,RLENGTH-RSTART+1);
+    printf("\`\`%s\`\`\n", MODEL);
+    print("***************************************");
+    printf(".. autoclass:: agntcy_acp.models.%s\n", MODEL);
+    print("   :members:");
+    print("   :undoc-members:");
+    print("   :show-inheritance:\n");
+}
+EOF
+done
